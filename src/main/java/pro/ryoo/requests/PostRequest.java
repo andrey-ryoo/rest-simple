4fd679bb89ed0d4906d258623656d23b7443618b
andrey.ryoo <andrey.ryoo@gmail.com>
2017-02-13 07:23:21 +0300

    Checkpoint

--- src/main/java/pro/ryoo/requests/PostRequest.java
+++ src/main/java/pro/ryoo/requests/PostRequest.java
@@ -3,25 +3,33 @@ package pro.ryoo.requests;
 import pro.ryoo.requests.base.Request;
 
 import java.io.File;
+import java.util.HashMap;
 import java.util.Map;
+import java.util.Optional;
 
 public class PostRequest extends Request {
 
     private Map<String, File> files;
-    private Map<String, Object> params;
+    private Map<String, Object> params = new HashMap<String, Object>();
 
     public PostRequest(String url) {
         this.url = url;
     }
 
     public PostRequest file(String name, File file) {
+        if (!Optional.ofNullable(files).isPresent()) {
+            files = new HashMap<String, File>();
+        }
         files.put(name, file);
         return this;
     }
 
     public PostRequest field(String name, Object value) {
         params.put(name, value);
+
         return this;
     }
+
+
 
 }
